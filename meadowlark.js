178a1ff760cee045b26607b1e23604cb66efb04f
Sean Burman <sean.burman@icloud.com>
2021-01-14 17:15:35 -0500

    Added handlers.js from exercises

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,4 @@
-const greeting = require('./lib/greeting')
+const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
@@ -15,23 +15,14 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-	res.render('about', { greeting: greeting.getGreeting() })
-})
+app.get('/about', handlers.about)
 
 //custom 404 page
-app.use((req, res) => {
-	res.status(404)
-	res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, req, res, next) => {
-	console.error(err.message)
-	res.status(500)
-	res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
 	`Express started on http://localhost:${port}; ` +
